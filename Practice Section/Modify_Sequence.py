af682baf28e08d3352fb582edddfddb70f15b772
Sanjeev <thedevelopersanjeev@gmail.com>
2018-02-05 18:59:56 +0530

    Added new problems

---
+++ Practice Section/Modify_Sequence.py
@@ -0,0 +1,9 @@
+n= int(input())
+num= list(map(int, input().split(' ')))
+single_num= 0
+for i in range(n):
+    single_num += num[i]*(10**i)
+if int(single_num)%11 ==0:
+    print("YES")
+else:
+    print('NO')
